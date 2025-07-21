21df4b97d443dbc5a9ac84e94e4fbb368b081111
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-21 13:16:14 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@
 mod rsvm;
 use crate::rsvm::*;
 
+use memmap2::Mmap;
+use std::fs::File;
+use std::path::Path;
+
 fn main() {
     let argv: Vec<String> = std::env::args().collect();
     let argc = argv.len();
     arg(0, &argv[0]);
     for (i, argv) in argv.iter().skip(1).enumerate() {
         arg(i + 1, argv);
+        let srcfile = File::open(Path::new(argv)).unwrap();
+        let src = unsafe { Mmap::map(&srcfile).unwrap() };
+        eprintln!("File size: {} bytes", src.len());
     }
     eprintln!("addr:{:?}", size_of::<Addr>());
 }
