fc7004f678eb89b4b6d950dddb7f8073e0ec4d26
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-23 17:12:02 +0400

    .

--- src/gui/config.rs
+++ src/gui/config.rs
@@ -1,6 +1,6 @@
 #![allow(non_camel_case_types)]
 
-type coord = u16;
+type coord = i16;
 
 /// screen width (mobile phone emulation)
 pub const W: coord = 240;
@@ -11,9 +11,9 @@ pub const H: coord = 320;
 pub const root_bg: sdl2::pixels::Color = sdl2::pixels::Color::RGB(0x22, 0x22, 0x33);
 // status bar background
 pub const status_bg: sdl2::pixels::Color = sdl2::pixels::Color::RGB(0x22, 0x11, 0x11);
-pub const status_clock: sdl2::pixels::Color = sdl2::pixels::Color::RGB(0x12, 0x34, 0x56);
+pub const status_clock: sdl2::pixels::Color = sdl2::pixels::Color::RGB(0xAA, 0xBB, 0xCC);
 // navigator bar background
 pub const navbar_bg: sdl2::pixels::Color = sdl2::pixels::Color::RGB(0x11, 0x22, 0x11);
 
 /// base font size
-pub const font_size: u8 = 10;
+pub const font_size: u16 = H as u16 / 0x10;
--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -8,6 +8,11 @@ use sdl2::rect::Rect;
 use sdl2::rwops::{self, RWops};
 use sdl2::ttf;
 
+use std::fmt::Write;
+use std::time::SystemTime;
+
+use crate::gui::config::font_size;
+
 pub struct GUI {
     sdl_context: sdl2::Sdl,
     window: sdl2::video::Window,
@@ -16,8 +21,8 @@ pub struct GUI {
     fps: std::time::Duration,
     status: Rect,
     navbar: Rect,
-    ttf_context: sdl2::ttf::Sdl2TtfContext,
-    font: ttf::Font<'static, 'static>,
+    ttf_context: &'static sdl2::ttf::Sdl2TtfContext,
+    font: &'static ttf::Font<'static, 'static>,
 }
 
 const SpaceMono_Regular: &[u8] = include_bytes!("../../static/font/SpaceMono-Regular.ttf");
@@ -42,15 +47,21 @@ impl GUI {
             .unwrap();
         let event_pump = sdl_context.event_pump().unwrap();
         let canvas = window.clone().into_canvas().build().unwrap();
-        let fps = std::time::Duration::new(0, 1_000_000_000u32 / 60);
-        let status = Rect::new(0, 0, uW, uH / 0x10);
-        let navbar = Rect::new(0, iH / 0x10 * 0x0F, uW, uH / 0x10);
-        let ttf_context = sdl2::ttf::init().unwrap();
+        let fps = std::time::Duration::new(1, 0); //1_000_000_000u32 / 60);
+        let status = Rect::new(0, 0, uW, font_size as u32);
+        let navbar = Rect::new(0, iH / 0x10 * 0x0F, uW, font_size as u32);
+        let ttf_context = {
+            let ttf_context = Box::new(sdl2::ttf::init().unwrap());
+            Box::leak(ttf_context)
+        };
         let font = {
             let rw = rwops::RWops::from_bytes(SpaceMono_Regular).unwrap();
-            ttf_context
-                .load_font_from_rwops(rw, config::font_size.into())
-                .unwrap()
+            let font = Box::new(
+                ttf_context
+                    .load_font_from_rwops(rw, config::font_size / 10 * 8)
+                    .unwrap(),
+            );
+            Box::leak(font)
         };
         Self {
             sdl_context,
@@ -94,6 +105,32 @@ impl GUI {
         // status bar
         self.canvas.set_draw_color(config::status_bg);
         self.canvas.fill_rect(self.status).unwrap();
+        // clock
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        let seconds = now.as_secs();
+        // render now string
+        let hours = (seconds / 3600) % 24;
+        let minutes = (seconds / 60) % 60;
+        let seconds = seconds % 60;
+        let mut time_str = String::new();
+        write!(&mut time_str, "{:02}:{:02}:{:02}", hours, minutes, seconds).unwrap();
+        let clock_surface = self
+            .font
+            .render(&time_str)
+            .blended(config::status_clock)
+            .unwrap();
+        let clock_texture = self
+            .canvas
+            .create_texture_from_surface(&clock_surface)
+            .unwrap();
+        let clock_query = clock_texture.query();
+        let w = clock_query.width;
+        let h = clock_query.height;
+        let x = self.status.w - w as i32;
+        let clock_rect = Rect::new(x, 0, w, h);
+        self.canvas.copy(&clock_texture, None, clock_rect).unwrap();
         // status bar
         self.canvas.set_draw_color(config::navbar_bg);
         self.canvas.fill_rect(self.navbar).unwrap();
