d567c6c28951ff6466753911eaa321a1ee936fdf
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-29 13:33:58 +0400

    .

--- src/gui.rs
+++ src/gui.rs
@@ -3,6 +3,10 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
+use sdl2::render::Canvas;
+use sdl2::keyboard::Keycode;
+use sdl2::event::Event;
+
 use crate::config::*;
 
 /// 2D coordinates
@@ -11,18 +15,18 @@ struct Coord(i16, i16);
 /// color: RGB TrueColor
 struct Color(u8, u8, u8);
 
-#[cfg(feature = "sdl")]
+// #[cfg(feature = "sdl")]
 pub struct GUI<'a> {
     title: &'a str,
     sdl_context: sdl2::Sdl,
     video_subsystem: sdl2::VideoSubsystem,
     window: sdl2::video::Window,
-    // canvas: sdl2::render::Canvas<sdl2::video::Window>,
+    canvas: sdl2::render::Canvas<sdl2::video::Window>,
     // texture_creator: sdl2::render::TextureCreator<sdl2::video::WindowContext>,
     // texture: sdl2::render::Texture<'a>,
 }
 
-#[cfg(feature = "sdl")]
+// #[cfg(feature = "sdl")]
 impl<'a> GUI<'a> {
     pub fn new(title: &'a str) -> Self {
         use crate::config;
@@ -33,13 +37,36 @@ impl<'a> GUI<'a> {
             .window(title, config::gui::width as u32, config::gui::height as u32)
             .build()
             .unwrap();
+        let canvas = window.clone().into_canvas().build().unwrap();
         GUI {
             title,
             sdl_context,
             video_subsystem,
             window,
+            canvas,
         }
     }
 
-    pub fn run(&mut self) {}
+    pub fn paint(&mut self) {
+        self.canvas.set_draw_color(sdl2::pixels::Color::RGB(0x22, 0x22, 0x22));
+        self.canvas.clear();
+        self.canvas.present();
+    }
+
+    pub fn run(&mut self) {
+        let mut event_pump = self.sdl_context.event_pump().unwrap();
+        'running: loop {
+            self.paint();
+            for event in event_pump.poll_iter() {
+                match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Q),
+                    ..
+                } => break 'running,
+                sdl2::event::Event::Quit { .. } => break 'running,
+                _ => {}
+            }
+        }}
+    }
 }
